4e47a0b9e544ad64a8b9cc9a63f7831f4b038880
Anthony Shaw <anthonyshaw@apache.org>
2018-06-01 19:38:08 +0100

    fix schema issue

--- CHANGES.md
+++ CHANGES.md
@@ -2,4 +2,4 @@
 
 ## 0.1.0
 
-- First release 
+- Initial Release of Infoblox integration
--- actions/update_object.py
+++ actions/update_object.py
@@ -4,7 +4,11 @@ __all__ = [
     'UpdateObjectAction',
 ]
 
-class CreateObjectAction(InfobloxBaseAction):
+class UpdateObjectAction(InfobloxBaseAction):
     def run(self, ref, **kwargs):
+        _keys = list(kwargs.keys())
+        for k in _keys:
+            if kwargs[k] is None:
+                del kwargs[k]
         result = self.connection.update_object(ref=ref, kwargs)
         return result
