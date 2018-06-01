b5cb2517f3136981d203474cbd1b1af935fbeafa
Anthony Shaw <anthonyshaw@apache.org>
2018-06-01 13:31:57 +0200

    rollback

--- actions/get_object.py
+++ actions/get_object.py
@@ -5,9 +5,8 @@ __all__ = [
 ]
 
 class GetObjectAction(InfobloxBaseAction):
-    def run(self, object_type, return_fields=None, **kwargs):
+    def run(self, object_type, **kwargs):
         result = self.connection.get_object(
             object_type,
-            return_fields=return_fields,
             payload=kwargs)
         return result
