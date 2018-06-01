793c2c45224e850e63b8f014abe1bc41b8482725
Jacques Clement <jacques.clement@dimensiondata.com>
2018-06-01 12:05:11 +0200

    Bug fix

--- actions/get_TLS_weather.py
+++ actions/get_TLS_weather.py
@@ -5,10 +5,9 @@ __all__ = [
 ]
 
 class GetTLSWeather():
-
     def __init__(self):
         pass
-        
+
     def run(self, **kwargs):
         result = requests.get('https://api.openweathermap.org/data/2.5/weather?q=Toulouse,fr&APPID=91a779c48c18e14f49b4d82ad2e4ff45')
         return result
