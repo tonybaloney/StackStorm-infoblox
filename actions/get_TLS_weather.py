189592309562315c5ca62c55d5c6812880d3ffc9
Jacques Clement <jacques.clement@dimensiondata.com>
2018-06-01 13:10:33 +0200

    Added JSON output

--- actions/get_TLS_weather.py
+++ actions/get_TLS_weather.py
@@ -11,4 +11,4 @@ class GetTLSWeather(Action):
 
     def run(self, **kwargs):
         result = requests.get('https://api.openweathermap.org/data/2.5/weather?q=Toulouse,fr&APPID=91a779c48c18e14f49b4d82ad2e4ff45')
-        return result
+        return result.json()
